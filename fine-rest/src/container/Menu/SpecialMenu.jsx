6f363ce4e4025b058a6c150963ab5c19fa1c782e
Abdullah Fareed <155915792+abdullahfareed01@users.noreply.github.com>
2025-10-02 09:28:47 +0500

    Update SpecialMenu.jsx

--- fine-rest/src/container/Menu/SpecialMenu.jsx
+++ fine-rest/src/container/Menu/SpecialMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { SubHeading, MenuItem } from "../../components";
 import { data, images } from "../../constants";
-import "./SpecialMenu.css";
+import "./specialmenu.css";
 
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">

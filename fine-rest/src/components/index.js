7fed6b414d71f55f40dd06774ea6569e44b73ea2
Abdullah Fareed <155915792+abdullahfareed01@users.noreply.github.com>
2025-10-02 08:46:13 +0500

    Update index.js

--- fine-rest/src/components/index.js
+++ fine-rest/src/components/index.js
@@ -2,6 +2,6 @@ import FooterOverlay from "./Footer/FooterOverlay";
 import Newsletter from "./Footer/Newsletter";
 import MenuItem from "./Menuitem/MenuItem";
 import Navbar from "./Navbar/Navbar";
-import SubHeading from "./SubHeading/Subheading";
+import SubHeading from "./SubHeading/SubHeading";
 
 export { FooterOverlay, Newsletter, MenuItem, Navbar, SubHeading };

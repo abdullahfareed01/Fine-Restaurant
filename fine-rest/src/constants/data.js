5cd3ad7ae62533792a0a6d4c08eef2e9573c42f6
Abdullah Fareed <your-email@example.com>
2025-10-11 03:33:24 +0500

    Updated Mneu Items

--- fine-rest/src/constants/data.js
+++ fine-rest/src/constants/data.js
@@ -1,16 +1,16 @@
 import images from "./images";
 
 export const wines = [
-  { title: "Chapel Hill Shiraz", price: "$56", tags: "AU | Bottle" },
+  { title: "Orange Jice Hill Shiraz", price: "$56", tags: "AU | Bottle" },
   { title: "Catena Malbee", price: "$59", tags: "AU | Bottle" },
-  { title: "La Vieillw Rose", price: "$44", tags: "FR | 750 ml" },
-  { title: "Rhino Pale Ale", price: "$31", tags: "CA | 750 ml" },
-  { title: "Irish Guinness", price: "$26", tags: "IE | 750 ml" },
+  { title: "La Vintage Rose", price: "$44", tags: "FR | 750 ml" },
+  { title: "Pale Blue Lemonade ", price: "$31", tags: "CA | 750 ml" },
+  { title: "Irish Lemons", price: "$26", tags: "IE | 750 ml" },
 ];
 
 export const cocktails = [
   { title: "Aperol Sprtiz", price: "$20", tags: "Aperol | prosecco | soda | 30 ml" },
-  { title: "Dark 'N' Stormy", price: "$16", tags: "Dark rum | Ginger beer | lime" },
+  { title: "Dark 'N' Stormy", price: "$16", tags: "Dark rum | Ginger Juice | lemonade" },
   { title: "Daiquiri", price: "$10", tags: "Rum | Citrus juice | Sugar" },
   { title: "Old Fashioned", price: "$31", tags: "Bourbon | Brown sugar | Bitters" },
   { title: "Negroni", price: "$26", tags: "Gin | Vermouth | Campari | Orange" },
